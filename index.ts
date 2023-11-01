7489620d3afea6bd94a8c5dd90e146da106a74cf
Eric Trinh <et.trinity@gmail.com>
2023-11-01 16:41:06 -0400

    Initial commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+import ical from "ical-generator";
+
+async function getCalendar() {
+  const response = await fetch("http://localhost:8080/?status=PENDING");
+  const reservations = await response.json();
+
+  const calendar = ical({
+    name: `Life Time Pending`,
+  });
+
+  reservations.forEach((r: any) => {
+    calendar.createEvent({
+      start: new Date(r.start),
+      end: new Date(r.end),
+      summary: r.name,
+      description: "",
+      location: "PENDING",
+      url: `https://my.lifetime.life/clubs/ny/sky-manhattan/classes/class-details.html?eventId=${r.lifetimeId}`,
+    });
+  });
+
+  return calendar;
+}
+const calendar = await getCalendar();
+Bun.write("pending.ics", calendar.toString());
