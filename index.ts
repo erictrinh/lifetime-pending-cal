e934b0d79d58d322083b4eb7984d1a6bea159994
Eric Trinh <et.trinity@gmail.com>
2023-11-05 13:44:45 -0500

    Update pending.ics

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import ical from "ical-generator";
 
 async function getCalendar() {
-  const response = await fetch("http://localhost:8080/?status=PENDING");
+  const response = await fetch("http://infinitie.local:8080/?status=PENDING");
   const reservations = await response.json();
 
   const calendar = ical({
@@ -19,7 +19,19 @@ async function getCalendar() {
     });
   });
 
-  return calendar;
+  const calendarJSON = reservations.map((r: any) => {
+    return {
+      start: new Date(r.start),
+      end: new Date(r.end),
+      summary: r.name,
+      description: "",
+      location: "PENDING",
+      url: `https://my.lifetime.life/clubs/ny/sky-manhattan/classes/class-details.html?eventId=${r.lifetimeId}`,
+    };
+  });
+
+  return { calendar, calendarJSON };
 }
-const calendar = await getCalendar();
+const { calendar, calendarJSON } = await getCalendar();
 Bun.write("pending.ics", calendar.toString());
+Bun.write("pending.json", JSON.stringify(calendarJSON, null, 2));
